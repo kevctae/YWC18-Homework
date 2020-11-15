5732ee127f5c5d83863bcbbbd360f12f9a0a1920
Kevin Tae <38150856+kevctae@users.noreply.github.com>
2020-11-15 17:28:24 +0700

    Fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app/home/home.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
