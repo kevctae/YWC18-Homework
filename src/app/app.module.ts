cb14d92923435ff1ed5e288b16867072f91fb50b
Kevin Tae <38150856+kevctae@users.noreply.github.com>
2020-11-16 15:47:14 +0700

    Bump all packages to latest version and install bootstrap and ng-boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './header/header.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HeaderModule,
     HttpClientModule,
-    
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
