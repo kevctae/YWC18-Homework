74703655044ab84f689dc6c90d2dd8ddb2d88e84
Kevin Tae <38150856+kevctae@users.noreply.github.com>
2020-11-16 19:47:22 +0700

    Template for content

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -13,7 +13,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -32,7 +32,6 @@ import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
     MatCardModule,
     MatRadioModule,
     FormsModule,
-    NgbAlertModule,
   ]
 })
 export class HeaderModule { }
